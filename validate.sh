cf5e045a7941465a03783c8b7aa77aa86b2185de
Tako Schotanus <tako@ceylon-lang.org>
2018-02-16 16:06:37 +0100

    feat(validation): Write script to validate booster catalog
    Added a `validate.sh` script that will download and run some code
    that will check the format and contents of the booster catlog.
    It will also retrieve all the bosoters so any errors should be
    obvious.
    
    Fixes #155

---
+++ validate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+GROUPID=io.fabric8.launcher
+ARTIFACTID=booster-catalog-service
+VERSION=24
+MAIN=io.fabric8.launcher.booster.BoosterValidator
+TARGET=target
+
+echo "Setting up the validator..."
+mvn -q dependency:copy -Dartifact=${GROUPID}:${ARTIFACTID}:${VERSION}:pom -DoutputDirectory=${TARGET}
+mvn -q dependency:copy -Dartifact=${GROUPID}:${ARTIFACTID}:${VERSION} -DoutputDirectory=${TARGET}
+mvn -q dependency:copy-dependencies -f ${TARGET}/${ARTIFACTID}-${VERSION}.pom -DoutputDirectory=dependencies
+java -cp ${TARGET}/\*:${TARGET}/dependencies/\* ${MAIN}
