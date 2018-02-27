990ceeb18f3548fed658cad552f0d26d02410e2a
George Gastaldi <gegastaldi@gmail.com>
2018-02-27 16:09:26 +0000

    Add cico_run_tests.sh

---
+++ cico_run_tests.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+. inherit-env
+
+yum -y update
+yum -y install centos-release-scl java-1.8.0-openjdk-devel git
+yum -y install rh-maven33
+
+scl enable rh-maven33 './validate.sh'
+
+if [ $? -ne 0 ]; then
+    echo 'Build Failed!'
+    exit 1
+fi
