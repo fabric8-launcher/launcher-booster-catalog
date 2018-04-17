b071f8be42d48f304ca42c8426cc7a7cb6b6faeb
George Gastaldi <gegastaldi@gmail.com>
2018-04-17 19:09:22 -0300

    fix: validate.sh now tests the PR author branch

--- validate.sh
+++ validate.sh
@@ -2,9 +2,12 @@
 
 set -e
 
+[ -f jenkins-env ] && cat jenkins-env | grep -e ghprbAuthorRepoGitUrl -e ghprbSourceBranch > inherit-env
+[ -f inherit-env ] && . inherit-env
+
 GROUPID=io.fabric8.launcher
 ARTIFACTID=booster-catalog-service
-VERSION=30
+VERSION=32
 MAIN=io.fabric8.launcher.booster.BoosterValidator
 TARGET=target
 
@@ -12,4 +15,4 @@ echo "Setting up the validator..."
 mvn -q dependency:copy -Dartifact=${GROUPID}:${ARTIFACTID}:${VERSION}:pom -DoutputDirectory=${TARGET}
 mvn -q dependency:copy -Dartifact=${GROUPID}:${ARTIFACTID}:${VERSION} -DoutputDirectory=${TARGET}
 mvn -q dependency:copy-dependencies -f ${TARGET}/${ARTIFACTID}-${VERSION}.pom -DoutputDirectory=dependencies
-java -cp ${TARGET}/\*:${TARGET}/dependencies/\* ${MAIN}
+java -cp ${TARGET}/\*:${TARGET}/dependencies/\* ${MAIN} ${ghprbAuthorRepoGitUrl} ${ghprbSourceBranch}
