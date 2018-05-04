bac33393cdd95e59c9cff98eae1213f7a25dc170
Ladislav Thon <lthon@redhat.com>
2018-05-04 17:30:32 +0200

    run booster catalog verifier as part of CI tests

--- cico_run_tests.sh
+++ cico_run_tests.sh
@@ -6,7 +6,7 @@ yum -y update
 yum -y install centos-release-scl java-1.8.0-openjdk-devel git
 yum -y install rh-maven33
 
-scl enable rh-maven33 './validate.sh'
+scl enable rh-maven33 './validate.sh && ./verify-all-boosters-build.sh'
 
 if [ $? -ne 0 ]; then
     echo 'Build Failed!'
