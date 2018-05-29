88e54a9352bcc7c72076ebf2a49280dca917eb61
Tako Schotanus <tako@ceylon-lang.org>
2018-05-29 20:28:53 +0200

    chore(script): Updated validator script and added `cat.sh`
    The validator script now uses version 36 and there's a new "cat" script that
    outputs the boosters from the catalog in processed form. (Meaning all common
    and environment overrides have been applied)

--- cat.sh
+++ cat.sh
@@ -2,17 +2,14 @@
 
 set -e
 
-[ -f jenkins-env ] && cat jenkins-env | grep -e ghprbAuthorRepoGitUrl -e ghprbSourceBranch > inherit-env
-[ -f inherit-env ] && . inherit-env
-
 GROUPID=io.fabric8.launcher
 ARTIFACTID=booster-catalog-service
 VERSION=36
-MAIN=io.fabric8.launcher.booster.BoosterValidator
+MAIN=io.fabric8.launcher.booster.BoosterCat
 TARGET=target
 
-echo "Setting up the validator..."
 mvn -q dependency:copy -Dartifact=${GROUPID}:${ARTIFACTID}:${VERSION}:pom -DoutputDirectory=${TARGET}
 mvn -q dependency:copy -Dartifact=${GROUPID}:${ARTIFACTID}:${VERSION} -DoutputDirectory=${TARGET}
 mvn -q dependency:copy-dependencies -f ${TARGET}/${ARTIFACTID}-${VERSION}.pom -DoutputDirectory=dependencies
-java -cp ${TARGET}/\*:${TARGET}/dependencies/\* ${MAIN} ${ghprbAuthorRepoGitUrl} ${ghprbSourceBranch}
+java -cp ${TARGET}/\*:${TARGET}/dependencies/\* ${MAIN} $*
+
