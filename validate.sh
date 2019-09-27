cb0fc0adbdcfc906acfdb226aeeb10228dd0252c
Tako Schotanus <tako@ceylon-lang.org>
2019-09-27 22:32:13 +0200

    feat: Added catalog file using new format
    Also updated the scripts to use latest BCS version

--- validate.sh
+++ validate.sh
@@ -2,12 +2,9 @@
 
 set -e
 
-[ -f jenkins-env ] && cat jenkins-env | grep -e ghprbAuthorRepoGitUrl -e ghprbSourceBranch > inherit-env
-[ -f inherit-env ] && . inherit-env
-
 GROUPID=io.fabric8.launcher
 ARTIFACTID=booster-catalog-service
-VERSION=58
+VERSION=60
 MAIN=io.fabric8.launcher.booster.BoosterValidator
 TARGET=target
 
@@ -15,4 +12,4 @@ echo "Setting up the validator..."
 mvn -q dependency:copy -Dartifact=${GROUPID}:${ARTIFACTID}:${VERSION}:pom -DoutputDirectory=${TARGET}
 mvn -q dependency:copy -Dartifact=${GROUPID}:${ARTIFACTID}:${VERSION} -DoutputDirectory=${TARGET}
 mvn -q dependency:copy-dependencies -f ${TARGET}/${ARTIFACTID}-${VERSION}.pom -DoutputDirectory=dependencies
-java -cp ${TARGET}/\*:${TARGET}/dependencies/\* ${MAIN} ${ghprbAuthorRepoGitUrl} ${ghprbSourceBranch}
+java -cp ${TARGET}/\*:${TARGET}/dependencies/\* ${MAIN}
